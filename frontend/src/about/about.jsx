37b5e13de9b8792dc1643466324b9566fb0cc6d8
maycomHoepers10 <maycomhoepers10@gmail.com>
2018-02-14 22:09:59 -0200

    Add files via upload
    Primeiro commite

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <p>teste teste teste teste teste teste</p>
+        <p>teste teste teste teste teste teste</p>
+        <p>teste teste teste teste teste teste</p>
+        <p>teste teste teste teste teste teste</p>
+        <p>teste teste teste teste teste teste</p>
+        <p>teste teste teste teste teste teste</p>
+        <p>teste teste teste teste teste teste</p>
+    </div>
+)
